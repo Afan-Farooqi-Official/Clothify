c73232c1c6089854a99361177662ea31a548c02a
Afan-Farooqi-Official <affanqaisar000@gmail.com>
2026-01-10 18:31:48 +0500

    admin complete

--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -5,7 +5,7 @@ const adminAuth = async (req, res, next) => {
         
         const { token } = req.headers
         if (!token) {
-            return res.json({success:false, message:"Not Authorized Login Again"})
+            return res.json({success:false, message:"Not Authorized, Login Again"})
         }
         const token_decode = jwt.verify(token,process.env.JWT_SECRET)
         if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
@@ -16,7 +16,7 @@ const adminAuth = async (req, res, next) => {
 
     } catch (error) {
         console.log(error);
-        res.json({success:true, message:error.message})
+        res.json({success:false, message:error.message})
     }
 }
 
